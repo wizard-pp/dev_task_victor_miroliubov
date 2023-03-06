a3c5191bbe4d77ef24dca53fca3c6c0594a32087
Victor Miroliubov <wizard@Perfects-iMac.local>
2023-03-06 10:39:42 +0300

    pulled right save result btn

--- views/order/_csv.php
+++ views/order/_csv.php
@@ -6,6 +6,10 @@ $queryParams = Yii::$app->request->queryParams;
 unset($queryParams['r']);
 ?>
 
-<p>
-    <?= Html::a(Yii::t('app', 'Save Result'), array_merge(['csv'], $queryParams), ['class' => 'btn btn-success']) ?>
-</p>
+<div class="row">
+    <div class="pull-right">
+        <p>
+            <?= Html::a(Yii::t('app', 'Save Result'), array_merge(['csv'], $queryParams), ['class' => 'btn btn-success']) ?>
+        </p>
+    </div>
+</div>
