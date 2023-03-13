d87c47601478a3bb93a1e9b27467ed8f3639709c
Victor Miroliubov <wizard@Perfects-iMac.local>
2023-03-13 10:05:49 +0300

    saving search form values after submitting

--- application/modules/order/views/default/_search.php
+++ application/modules/order/views/default/_search.php
@@ -8,8 +8,11 @@ use yii\helpers\Url;
 $orderSearch = Yii::$app->request->get('OrderSearch');
 $isAllOrders = is_null($orderSearch)|| !isset($orderSearch['status']);
 $queryParams = Yii::$app->request->queryParams;
-unset($queryParams['r']);
-$queryParams[0] = '/' . Yii::$app->controller->getRoute();
+$targetQueryParams = $queryParams;
+unset($targetQueryParams['r']);
+$targetQueryParams[0] = '/' . Yii::$app->controller->getRoute();
+
+$searchValue = $queryParams['OrderSearch']['id'] ?? $queryParams['OrderSearch']['link'] ?? $queryParams['OrderSearch']['username'] ?? '';
 ?>
 
 <ul class="nav nav-tabs p-b">
@@ -19,7 +22,7 @@ $queryParams[0] = '/' . Yii::$app->controller->getRoute();
         </a>
     </li>
     <?php foreach (Order::find()->select('status')->distinct()->all() as $status): ?>
-        <?php $targetQueryParams = array_replace_recursive($queryParams, ['OrderSearch' => ['status' => $status->status]]); ?>
+        <?php $targetQueryParams = array_replace_recursive($targetQueryParams, ['OrderSearch' => ['status' => $status->status]]); ?>
         <?php $isActive = isset($orderSearch['status']) && $orderSearch['status'] == $status->status; ?>
         <li <?php if ($isActive): ?> class="active" <?php endif; ?>>
             <a href="<?= Url::toRoute($targetQueryParams) ?>">
@@ -31,13 +34,17 @@ $queryParams[0] = '/' . Yii::$app->controller->getRoute();
     <li class="pull-right custom-search">
         <form id="search-form" class="form-inline" action="<?= Url::current() ?>" method="get">
             <div class="input-group">
-                <input type="text" name="search" class="form-control" value="" placeholder="Search orders">
+                <input type="text"
+                       name="search"
+                       class="form-control"
+                       value="<?= $searchValue ?>"
+                       placeholder="<?= Yii::t('app', 'Search orders') ?>">
                 <span class="input-group-btn search-select-wrap">
 
             <select class="form-control search-select" name="search-type">
-              <option value="OrderSearch[id]" selected="">Order ID</option>
-              <option value="OrderSearch[link]">Link</option>
-              <option value="OrderSearch[username]">Username</option>
+              <option value="OrderSearch[id]" <?php if (!empty($queryParams['OrderSearch']['id'])): ?> selected="" <?php endif; ?>>Order ID</option>
+              <option value="OrderSearch[link]" <?php if (!empty($queryParams['OrderSearch']['link'])): ?> selected="" <?php endif; ?>>Link</option>
+              <option value="OrderSearch[username]" <?php if (!empty($queryParams['OrderSearch']['username'])): ?> selected="" <?php endif; ?>>Username</option>
             </select>
             <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
             </span>
@@ -53,8 +60,10 @@ $('#search-form').on('submit', function (e) {
 
     let paramName = $(this).find('[name=search-type]').val();
     let paramValue = $(this).find('[name=search]').val();
-
+    
     let href = $(this).attr('action');
+    href = removeUrlParams(href, ['OrderSearch\[id\]', 'OrderSearch\[link\]', 'OrderSearch\[username\]']);
+
     href = replaceUrlParam(href, paramName, paramValue);
 
     window.location.href = href;
--- application/web/js/custom.js
+++ application/web/js/custom.js
@@ -10,3 +10,31 @@ function replaceUrlParam(url, paramName, paramValue)
     url = url.replace(/[?#]$/,'');
     return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
 }
+
+function removeUrlParams(urlString, paramNamesArray)
+{
+    paramNamesArray.forEach(paramName => urlString = removeUrlParam(urlString, paramName));
+
+    return urlString;
+}
+
+function removeUrlParam(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
